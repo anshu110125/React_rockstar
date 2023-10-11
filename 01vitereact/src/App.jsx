597d123489caad9e7fe68727a210d9d1b54ea068
anshu kumar <anshuzbd2000@gmail.com>
2023-10-11 14:08:37 +0530

    code added

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,19 @@
+
+import Chai from "./Chai"
+
+function App() {
+  return (
+    // <h1>
+    //     chai or react with vite|AK
+    // </h1>
+    <>
+     <Chai/>
+    <h1>chai aur react</h1>
+    <p>test para</p>
+    </>
+   
+
+  )
+}
+
+export default App
