87023793134188726331eb92e1387d4e999e2572
anshu kumar <anshuzbd2000@gmail.com>
2023-10-13 01:18:22 +0530

    interview

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,13 +2,15 @@
 import Chai from "./Chai"
 
 function App() {
+
+  const username="chai or code"
   return (
     // <h1>
     //     chai or react with vite|AK
     // </h1>
     <>
      <Chai/>
-    <h1>chai aur react</h1>
+    <h1>chai aur react {username}</h1>
     <p>test para</p>
     </>
    
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { createElement } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function Myapp(){
+    return(
+        <div>
+ <h3>custom react to main</h3>
+        </div>
+       
+    
+    )
+    }
+    // const reactElement = {
+    //     type: 'a',
+    //     props: {
+    //         href: 'https://google.com',
+    //         target: '_blank'
+    //     },
+    //     children: 'Click me to visit google'
+    // }
+    
+    const anElement=(
+        <a href="https://google.com" target='_blank'>Visit google</a> 
+    )
+    const reactElement=React.createElement(
+        'a',
+        {href:'http://google.com',target:'_blank'},
+        'click to visit google'
+    )
+
+    
 
 ReactDOM.createRoot(document.getElementById('root')).render(
- 
-    <App />
-  
+     <App/>
 )
